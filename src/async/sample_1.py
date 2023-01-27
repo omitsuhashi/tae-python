5ad136578a4067c24601460ee026d07713eb9a8b
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2023-01-27 11:45:06 +0900

    comment reason of `time.sleep` cannot async

--- src/async/sample_1.py
+++ src/async/sample_1.py
@@ -25,6 +25,8 @@ async def main():
     async with asyncio.TaskGroup() as tg:
         for i in range(10):
             # couldn't create my async procedure
+            # below link is reason why
+            # https://qiita.com/everylittle/items/57da997d9e0507050085#:~:text=asyncio.sleep()%20%E3%82%92,%E3%81%97%E3%81%9D%E3%81%86%E3%81%AA%E4%BE%8B%E3%80%82
             # val = tg.create_task(async_square_with_sleep(i))
             task = tg.create_task(async_square_with_io_sleep(i))
             tasks.append(task)
